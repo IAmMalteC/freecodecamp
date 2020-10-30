3b7731a2a5a8d2be9401f21736d13277c5569152
Malte <s0563114@htw-berlin.de>
2020-10-30 14:48:55 +0100

    refactor test to use regex

--- js/scripts/romanNumberConverter.test.js
+++ js/scripts/romanNumberConverter.test.js
@@ -2,44 +2,44 @@ const { test } = require("@jest/globals");
 const convertToRoman = require("./romanNumberConverter");
 
 test('Check if 2 returns "II"', () => {
-  expect(convertToRoman(2)).toBe("II");
+  expect(convertToRoman(2)).toMatch(/II/);
 });
 test('Check if 4 returns "IV"', () => {
-  expect(convertToRoman(4)).toBe("IV");
+  expect(convertToRoman(4)).toMatch(/IV/);
 });
 test('Check if 5 returns "V"', () => {
-  expect(convertToRoman(5)).toBe("V");
+  expect(convertToRoman(5)).toMatch(/V/);
 });
 test('Check if 9 returns "IX"', () => {
-  expect(convertToRoman(9)).toBe("IX");
+  expect(convertToRoman(9)).toMatch(/IX/);
 });
 test('Check if 10 returns "X"', () => {
-  expect(convertToRoman(10)).toBe("X");
+  expect(convertToRoman(10)).toMatch(/X/);
 });
 test('Check if 29 returns "XXIX', () => {
-  expect(convertToRoman(29)).toBe("XXIX");
+  expect(convertToRoman(29)).toMatch(/XXIX/);
 });
 test('Check if 44 returns "XLIV"', () => {
-  expect(convertToRoman(44)).toBe("XLIV");
+  expect(convertToRoman(44)).toMatch(/XLIV/);
 });
 test('Check if 68 returns "LXVIII"', () => {
-  expect(convertToRoman(68)).toBe("LXVIII");
+  expect(convertToRoman(68)).toMatch(/LXVIII/);
 });
 test('Check if 99 returns "XCIX"', () => {
-  expect(convertToRoman(99)).toBe("XCIX");
+  expect(convertToRoman(99)).toMatch(/XCIX/);
 });
 test('Check if 400 returns "CD"', () => {
-  expect(convertToRoman(400)).toBe("CD");
+  expect(convertToRoman(400)).toMatch(/CD/);
 });
 test('Check if 500 returns "D"', () => {
-  expect(convertToRoman(500)).toBe("D");
+  expect(convertToRoman(500)).toMatch(/D/);
 });
 test('Check if 798 returns "DCCXCVIII"', () => {
-  expect(convertToRoman(798)).toBe("DCCXCVIII");
+  expect(convertToRoman(798)).toMatch(/DCCXCVIII/);
 });
 test('Check if 1000 returns "M"', () => {
-  expect(convertToRoman(1000)).toBe("M");
+  expect(convertToRoman(1000)).toMatch(/M/);
 });
 test('Check if 3999 returns "MMMCMXCIX"', () => {
-  expect(convertToRoman(3999)).toBe("MMMCMXCIX");
+  expect(convertToRoman(3999)).toMatch(/MMMCMXCIX/);
 });
