17a3f0893a7002c9df9dc228eb0b4d61e46b4394
Malte <s0563114@htw-berlin.de>
2020-10-31 11:58:08 +0100

    passes test to transform 'B' to 'O'

--- js/scripts/caesarsCipher.js
+++ js/scripts/caesarsCipher.js
@@ -4,8 +4,17 @@ function rot13(str) {
   const stepsize = 13;
   // find current ascii value
   let char = ascii(str[0]);
-  // reduce by the prefered stepsize
-  char -= stepsize;
+  // transform by the prefered stepsize
+  if (char < 65 || char > 90) {
+    // if char isn't an uppercase character, keep the value
+    char = char;
+  } else if (char > 78) {
+    // if char is bigger then the value for N (78), subtract the value.
+    char -= stepsize;
+  } else {
+    // if char is smaller then the value for N (78), add 26 to subtract the stepsize and staying in the range.
+    char = char + 26 - stepsize;
+  }
   // reformat to str
   char = String.fromCharCode(char);
   // and return
