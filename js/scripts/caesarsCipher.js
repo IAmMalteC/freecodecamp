8b4ce5a05607eddad8f434869a6d22969f803fcc
Malte <s0563114@htw-berlin.de>
2020-10-31 13:03:15 +0100

    minor refactoring

--- js/scripts/caesarsCipher.js
+++ js/scripts/caesarsCipher.js
@@ -1,28 +1,23 @@
 function rot13(str) {
   // Function to get the ascii number
-  const ascii = (a) => a.charCodeAt(0);
-  const stepsize = 13;
+  const offset = 13;
   let decryptedMessage = "";
-  let char;
   // Decipher
   for (let i = 0; i < str.length; i++) {
     // find current ascii value
-    char = ascii(str[i]);
+    let char = str[i].charCodeAt(0);
     // transform by the prefered stepsize
     if (char < 65 || char > 90) {
       // if char isn't an uppercase character, keep the value
       char = char;
     } else if (char > 77) {
       // if char is bigger then the value for M (77), subtract the value.
-      char -= stepsize;
+      char -= offset;
     } else {
       // if char is smaller then the value for M (77), add 26 to subtract the stepsize and staying in the range.
-      char = char + 26 - stepsize;
+      char = char + 26 - offset;
     }
-    // reformat to str
-    char = String.fromCharCode(char);
-    // add to message
-    decryptedMessage += char;
+    decryptedMessage += String.fromCharCode(char);
   }
   return decryptedMessage;
 }
