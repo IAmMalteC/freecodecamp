2c7b0f6e454bee68450cc1328d229903aaab1a34
Malte <s0563114@htw-berlin.de>
2020-10-31 11:41:07 +0100

    passes first char test

--- js/scripts/caesarsCipher.js
+++ js/scripts/caesarsCipher.js
@@ -1,5 +1,17 @@
-function rot13(str) {}
+function rot13(str) {
+  // Function to get the ascii number
+  const ascii = (a) => a.charCodeAt(0);
+  const stepsize = 13;
+  // find current ascii value
+  let char = ascii(str[0]);
+  // reduce by the prefered stepsize
+  char -= stepsize;
+  // reformat to str
+  char = String.fromCharCode(char);
+  // and return
+  return char;
+}
 
-rot13("SERR PBQR PNZC");
+rot13("N");
 
 module.exports = rot13;
