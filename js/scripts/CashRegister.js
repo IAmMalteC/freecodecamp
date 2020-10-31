7e178536391f19782b78c5293d22b1cb594c602c
Malte <s0563114@htw-berlin.de>
2020-10-31 17:12:25 +0100

    added tests and first try

--- js/scripts/CashRegister.js
+++ js/scripts/CashRegister.js
@@ -1,18 +1,41 @@
+function sumCid(cid) {
+  let sum = 0;
+  for (let i = 0; i < cid.length; i++) {
+    sum += cid[i][1];
+  }
+  return sum;
+}
+
 function checkCashRegister(price, cash, cid) {
-  var change;
-  return change;
+  let answer = { status: "", change: [] };
+  let possible_states = ["OPEN", "CLOSED", "INSUFFICIENT_FUNDS"];
+  // Calculate total amount in the cid
+  let cidSum = sumCid(cid);
+  let change = cash - price;
+  // Perform the three checks
+  // *First - Check if the sum of the cid is sufficient
+  if (change > cidSum) {
+    answer.status = possible_states[2];
+    return answer;
+  } else if (change === cidSum) {
+    answer.status = possible_states[1];
+    answer.change = cid;
+    return answer;
+  } else {
+    return answer;
+  }
 }
 
 checkCashRegister(19.5, 20, [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100],
+  ["PENNY", 0.01],
+  ["NICKEL", 0],
+  ["DIME", 0],
+  ["QUARTER", 0],
+  ["ONE", 1],
+  ["FIVE", 0],
+  ["TEN", 0],
+  ["TWENTY", 0],
+  ["ONE HUNDRED", 0],
 ]);
 
 module.exports = checkCashRegister;
--- js/scripts/CashRegister.test.js
+++ js/scripts/CashRegister.test.js
@@ -1,1 +1,108 @@
 const checkCashRegister = require("./CashRegister");
+
+test('Return {status: "OPEN", change: [["QUARTER", 0.5]] }', () => {
+  const expected = { status: "OPEN", change: [["QUARTER", 0.5]] };
+  expect(
+    checkCashRegister(19.5, 20, [
+      ["PENNY", 1.01],
+      ["NICKEL", 2.05],
+      ["DIME", 3.1],
+      ["QUARTER", 4.25],
+      ["ONE", 90],
+      ["FIVE", 55],
+      ["TEN", 20],
+      ["TWENTY", 60],
+      ["ONE HUNDRED", 100],
+    ])
+  ).toMatchObject(expected);
+});
+test('Return {status: "OPEN", change: ... }', () => {
+  const expected = {
+    status: "OPEN",
+    change: [
+      ["TWENTY", 60],
+      ["TEN", 20],
+      ["FIVE", 15],
+      ["ONE", 1],
+      ["QUARTER", 0.5],
+      ["DIME", 0.2],
+      ["PENNY", 0.04],
+    ],
+  };
+  expect(
+    checkCashRegister(3.26, 100, [
+      ["PENNY", 1.01],
+      ["NICKEL", 2.05],
+      ["DIME", 3.1],
+      ["QUARTER", 4.25],
+      ["ONE", 90],
+      ["FIVE", 55],
+      ["TEN", 20],
+      ["TWENTY", 60],
+      ["ONE HUNDRED", 100],
+    ])
+  ).toMatchObject(expected);
+});
+
+test('Return {status: "INSUFFICIENT_FUNDS", change: [] }', () => {
+  const expected = { status: "INSUFFICIENT_FUNDS", change: [] };
+  expect(
+    checkCashRegister(19.5, 20, [
+      ["PENNY", 0.01],
+      ["NICKEL", 0],
+      ["DIME", 0],
+      ["QUARTER", 0],
+      ["ONE", 0],
+      ["FIVE", 0],
+      ["TEN", 0],
+      ["TWENTY", 0],
+      ["ONE HUNDRED", 0],
+    ])
+  ).toMatchObject(expected);
+});
+test('Return {status: "INSUFFICIENT_FUNDS", change: [] }', () => {
+  const expected = { status: "INSUFFICIENT_FUNDS", change: [] };
+  expect(
+    checkCashRegister(19.5, 20, [
+      ["PENNY", 0.01],
+      ["NICKEL", 0],
+      ["DIME", 0],
+      ["QUARTER", 0],
+      ["ONE", 1],
+      ["FIVE", 0],
+      ["TEN", 0],
+      ["TWENTY", 0],
+      ["ONE HUNDRED", 0],
+    ])
+  ).toMatchObject(expected);
+});
+
+test('Return {status: "CLOSED", change: ... }', () => {
+  const expected = {
+    status: "CLOSED",
+    change: [
+      ["PENNY", 0.5],
+      ["NICKEL", 0],
+      ["DIME", 0],
+      ["QUARTER", 0],
+      ["ONE", 0],
+      ["FIVE", 0],
+      ["TEN", 0],
+      ["TWENTY", 0],
+      ["ONE HUNDRED", 0],
+    ],
+  };
+  expect(
+    checkCashRegister(19.5, 20, [
+      ["PENNY", 0.5],
+      ["NICKEL", 0],
+      ["DIME", 0],
+      ["QUARTER", 0],
+      ["ONE", 0],
+      ["FIVE", 0],
+      ["TEN", 0],
+      ["TWENTY", 0],
+      ["ONE HUNDRED", 0],
+    ])
+  ).toMatchObject(expected);
+});
